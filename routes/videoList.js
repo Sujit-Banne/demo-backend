95ce6271203cda9766d93e9a9f8bb3d928cf8590
Sujit Banne <sujitbanne13@gmail.com>
2023-03-25 21:47:28 +0530

    videolist

--- routes/videoList.js
+++ routes/videoList.js
@@ -17,4 +17,23 @@ router.get('/api/videolist', (req, res, next) => {
         })
 })
 
+
+router.delete('/api/videos/:id', (req, res, next) => {
+    const id = req.params.id;
+    VideoDetails
+        .deleteOne({ _id: id })
+        .exec()
+        .then(result => {
+            res.status(200).json({
+                message: 'Video deleted successfully'
+            })
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            })
+        })
+})
+
+
 module.exports = router
